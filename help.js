aa5a83f6bc83e9d3c4578f1bb89088b739b59140
Daniele Sabre <daniele.sabre@gmail.com>
2018-11-23 08:52:08 +0100

    Fix

--- help.js
+++ help.js
@@ -11,12 +11,12 @@ module.exports.help = function(){
 	}
 	
 	const table = new Table({
-		head : header,
-		chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-			, 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-			, 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-			, 'right': '' , 'right-mid': '' , 'middle': ' ' },
-		style: { 'padding-left': 0, 'padding-right': 0 }
+		head : header
+		//,chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+		//	, 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+		//	, 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+		//	, 'right': '' , 'right-mid': '' , 'middle': ' ' },
+		//style: { 'padding-left': 0, 'padding-right': 0 }
 	});
 	
 	table.push([
